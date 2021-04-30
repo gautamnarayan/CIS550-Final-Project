25ff34f587e1d097d87db5d1f15a3a62de45dd80
Gautam Narayan <gnarayan@seas.upenn.edu>
2021-04-30 16:56:17 -0400

    Initial map committ

---
+++ client/src/components/Map.js
@@ -0,0 +1,66 @@
+import React from "react";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker
+} from "react-simple-maps";
+
+export default class Map extends React.Component {
+    constructor(props) {
+		super(props);
+
+		this.state = {
+			markers: [],
+		};
+	};
+
+    componentDidMount() {
+		this.setState({
+			markers: []
+		});
+    }
+
+    render() {
+      return (
+        <ComposableMap projection="geoAlbersUsa">
+          <Geographies geography={"https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"}>
+            {({ geographies }) => (
+              <>
+                {geographies.map(geo => (
+                  <Geography
+                    key={geo.rsmKey}
+                    stroke="#FFF"
+                    geography={geo}
+                    fill="#DDD"
+                  />
+                ))}
+              </>
+            )}
+          </Geographies>
+              {[].map(({ name, coordinates, markerOffset }) => (
+            <Marker key={name} coordinates={coordinates}>
+              <g
+                fill="none"
+                stroke="#FF5533"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                transform="translate(-12, -24)"
+              >
+                <circle cx="12" cy="10" r="3" />
+                <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
+              </g>
+              <text
+                textAnchor="middle"
+                y={markerOffset}
+                style={{ fontFamily: "system-ui", fill: "#5D5A6D" }}
+              >
+                {name}
+              </text>
+            </Marker>
+          ))}
+        </ComposableMap>
+      );
+    };
+}
--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PageNavbar from './PageNavbar';
+import Map from './Map'
 
 export default class Results extends React.Component {
 	render() {
 		return (
 			<div className="Results">
 				<PageNavbar />
+				<br/>
+				<Map/>
 			</div>
 		);
 	};
